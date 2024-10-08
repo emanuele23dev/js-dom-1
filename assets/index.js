8fe199379c57d068f5b12b2387a7fc7557d4c1aa
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-08 17:51:39 +0200

    end js dom

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,18 @@
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
-const buttonEl = document.getElementById("button");
-const lampadaSpenta = document.getElementById("lampada1");
-const lampadaAccesa = new Image();
+const lampadaSpenta = document.getElementById("img1");
+const buttonEl = document.getElementById("myButton");
 
-buttonEl.addEventListener("click", () => {
-  lampadaAccesa.src = "./img/yellow_lamp.png";
-  lampadaSpenta.style.backgroundImage = `url(${lampadaAccesa.src})`;
+buttonEl.style.borderRadius = '2rem'
+buttonEl.style.padding = "10 20px"
+buttonEl.style.backgroundColor = '#006eff'
+buttonEl.style.color = "white";
+buttonEl.style.border = "none";
+
+
+
+buttonEl.addEventListener("click", function () {
+  buttonEl.textContent = "Spegni";
+  lampadaSpenta.src = "./assets/img/yellow_lamp.png";
 });
